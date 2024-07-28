541f2b973319eef44032b33f885c3edd693b6083
kitamreik <kitdamreik@gmail.com>
2024-07-28 03:20:49 -0600

    init templates

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -10,7 +10,8 @@ from flask import Flask, jsonify, url_for, request, render_template
 from flask_cors import CORS #update after installing cors and setting up react boilerplate
 
 app = Flask(__name__, static_folder='static') # Next we create an instance of this class. The first argument is the name of the application’s module or package. __name__ is a convenient shortcut for this that is appropriate for most cases. This is needed so that Flask knows where to look for resources such as templates and static files.
-CORS(app)
+
+CORS(app) #enable cors at all routes
 
 @app.route('/') #We then use the route() decorator to tell Flask what URL should trigger our function.
 def home():
@@ -34,12 +35,26 @@ def error():
 def get_data():
     return jsonify({'message':'This is the get route for retrieving the data'})
 
+# @app.route('/api/data', methods=['POST']) 
+# def post_data():
+#     data = request.json
+#     #referring to react form - title/content
+#     title = data.get('title')
+#     content = data.get('content')
+#     return jsonify({'title': title, 'content': content}), 200
+
+
 @app.route('/api/data/create', methods=['GET', 'POST']) 
 def post_data():
     if request.method == 'POST':
-        return 'Success'
+        print("success")
+        data = request.json
+        #referring to react form - title/content
+        title = data.get('title')
+        content = data.get('content')
+        return jsonify({'title': title, 'content': content}), 200
     else:
-        return jsonify({'message':'This is the create route for posting the data'})
+        return jsonify({'message':'Error retrieving data'})
 
 if __name__ == '__main__':
     app.run(debug=True)
