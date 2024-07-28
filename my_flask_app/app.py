9384a9332d1df911db4ab3d628542926673c8442
kitamreik <kitdamreik@gmail.com>
2024-07-27 22:59:16 -0600

    testing static files

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,21 +1,32 @@
-from flask import Flask, jsonify, url_for, request
+from flask import Flask, jsonify, url_for, request, send_from_directory
+
+#package explanation
+# jsonify - sends json messages to routes
+# url_for - adding dynamic routing (out of scope)
+# request - using form methods/transmitting data
+# send_from_directory - render HTML files
+# render_template - use Templating Engine (Jinja2)
+
 from flask_cors import CORS #update after installing cors and setting up react boilerplate
 
-app = Flask(__name__) # Next we create an instance of this class. The first argument is the name of the application’s module or package. __name__ is a convenient shortcut for this that is appropriate for most cases. This is needed so that Flask knows where to look for resources such as templates and static files.
+app = Flask(__name__, static_folder='static') # Next we create an instance of this class. The first argument is the name of the application’s module or package. __name__ is a convenient shortcut for this that is appropriate for most cases. This is needed so that Flask knows where to look for resources such as templates and static files.
 CORS(app)
 
 @app.route('/') #We then use the route() decorator to tell Flask what URL should trigger our function.
 def home():
-    return jsonify({'message':'Hello, World! This is the index page for our pending app'})
+    return send_from_directory(app.static_folder, 'index.html')
+    #return jsonify({'message':'Hello, World! This is the index page for our pending app'})
 
 @app.route('/about')
 def about():
-    return jsonify({'message':'Here is a place where you can learn more about our hackathon team'})
+    return send_from_directory('static', 'about.html')
+    #return jsonify({'message':'Here is a place where you can learn more about our hackathon team'})
 
 
 @app.route('/error') 
 def error():
-    return jsonify({'error':'Uh oh, page not found.'})
+    return send_from_directory('static', 'error.html')
+    #return jsonify({'error':'Uh oh, page not found.'})
 
 #Mock API:
 
